0c28a7d4c7a6f26c9155946dd345dd41b03d6ad1
Gaurav-023 <gauravsinghania812@gmail.com>
2026-05-13 22:05:16 +0530

    Solved DSA Problem in JS

---
+++ week1/easy/anagram.js
@@ -0,0 +1,22 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+function isAnagram(str1, str2) {
+    const value =str1.toLowerCase().split("").sort().join("");
+    const value2 = str2.toLowerCase().split("").sort().join("");
+    if(value==value2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+
+module.exports = isAnagram;
